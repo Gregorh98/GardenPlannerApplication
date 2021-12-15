67dbf50006fecc6e1b72ab77c0a7af1be3a69693
Gregor <gregor.hastings@outlook.com>
2021-12-15 16:41:20 +0000

    Made square-foot plots clickable with reference

--- main.py
+++ main.py
@@ -21,38 +21,49 @@ class Settings:
 
 	def mapGarden(self):
 		self.getDimensions()
-		gardenMap = []
-		for y in range(self.height):
-			gardenMap.append([])
-			for x in range(self.width):
-				gardenMap[y].append(Plot(x, y))
-		
-		print(gardenMap)
+		self.gardenMap = []
 		self.drawGardenMap()
 	
 	def drawGardenMap(self):
 		newWindow = Toplevel(self.root)
 		
-		tileWidth = 32 #The dimensions of the representation of one square foot of land
+		tileWidth = 64 #The dimensions of the representation of one square foot of land
 		
-		c = Canvas(newWindow, background="#009A17", height=((2*tileWidth)+self.height*tileWidth), width=((2*tileWidth)+self.width*tileWidth))
+		c = Canvas(newWindow, background="#2c3c1e", height=((2*tileWidth)+self.height*tileWidth), width=((2*tileWidth)+self.width*tileWidth))
 		c.pack()
 		
 		for y in range(self.height):
+			self.gardenMap.append([])
 			for x in range(self.width):
-				#Define object pass parameters tile width, x, y and then add to garden map
-				#Draw on GUI as part of internal def		
-				c.create_rectangle(tileWidth+x*tileWidth, tileWidth+y*tileWidth, tileWidth+x*tileWidth+tileWidth, tileWidth+y*tileWidth+tileWidth, fill="brown")
-				
+				self.gardenMap[y].append(Plot(c, x, y, tileWidth))
+				c.tag_bind(self.gardenMap[y][x].canvasElement, "<Button-1>", self.gardenMap[y][x].outputName)
+						
+		# id1=str([99,99])
+		# print(id1)
+		# c.create_rectangle(0, 0, 64, 64, fill="red", tags=id1)
+		# c.tag_bind(id1, "<Button-1>", self.hello)
+		
+		# print(self.gardenMap)
+		# for x in self.gardenMap[0]:
+			# print(x.id)
 		
 	def getDimensions(self):
 		self.width	= int(self.widthEntryBox.get())
 		self.height = int(self.heightEntryBox.get())
 
+
+
 class Plot():
-	def __init__(self, x, y):
+	def __init__(self, canvas, x, y, tileWidth):
 		self.x = x
 		self.y = y
+		self.id = str([x,y])
+		self.canvasElement = canvas.create_rectangle(tileWidth+x*tileWidth, tileWidth+y*tileWidth, tileWidth+x*tileWidth+tileWidth, tileWidth+y*tileWidth+tileWidth, fill="#52402a", outline="#482f1f")
+		
+		
+	def outputName(self, args):
+		print(self.id)
+		
 
 A = Settings()
 A.root.mainloop()
