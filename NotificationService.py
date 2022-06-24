f4604ce6081ac1604b737232d9414eebec9d7135
Gregor Hastings <G.Hastings@almis.co.uk>
2022-06-24 14:16:20 +0100

    - Initial Commit - Added summary

---
+++ NotificationService.py
@@ -0,0 +1,11 @@
+"""
+Notification Service
+
+This will run in the background and provide updates via email in relation to your plants.
+
+- Weekly summary of how plants are growing
+- Watering reminders (after period of increased dryness)
+- Fertiliser reminders
+- Harvest alert (for ready crops)
+- Planting alert (for planned crops)
+"""
